b7d271f2783181846f21b951f52d37f4d98eb285
Yasmin <yasminor@gmail.com>
2016-10-11 14:57:04 -0700

    Added conventional actions for the albums and books controllers

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,53 @@
 class AlbumsController < ApplicationController
+
+  def index
+    @albums = Album.all
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def new
+    @album = Album.new
+  end
+
+  def edit
+    @album = Album.find(params[:id])
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      #SUCCESS
+      redirect_to albums_path
+    else
+      #UNSUCCESSFUL
+      render :new
+    end
+  end
+
+  def update
+    @album = Album.find(params[:id])
+        if @album.update(album_params)
+          # SUCCESS
+          redirect_to albums_path
+        else
+          # UNSUCCESSFUL
+          render :edit
+        end
+  end
+
+  def destroy
+    Album.find(params[:id]).destroy
+    redirect_to albums_path
+  end
+
+  private
+
+  # def albums_params
+  #   params.require(:album).permit(:name, :director, :description)
+  # end
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,53 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      #SUCCESS
+      redirect_to books_path
+    else
+      #UNSUCCESSFUL
+      render :new
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+        if @book.update(book_params)
+          # SUCCESS
+          redirect_to books_path
+        else
+          # UNSUCCESSFUL
+          render :edit
+        end
+  end
+
+  def destroy
+    Book.find(params[:id]).destroy
+    redirect_to books_path
+  end
+
+  private
+
+  # def books_params
+  #   params.require(:book).permit(:name, :director, :description)
+  # end
+
+
 end
