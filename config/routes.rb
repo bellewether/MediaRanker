cd60a26b0a1484c1f95f2fd8e2e05e07f065d5dc
Yasmin <yasminor@gmail.com>
2016-10-11 14:12:02 -0700

    Added a movies controller, added another movies route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :home, only: [:index]
 
-  
+  get '/movies' => 'movies#index'
+  get 'movies/new' => 'movies#new'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
