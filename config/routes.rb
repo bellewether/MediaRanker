27485021535d6ddec7c1946b6dba53ddba0aa539
Yasmin <yasminor@gmail.com>
2016-10-11 14:48:28 -0700

    Generated albums and books controllers and added RESTful routes for both

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :home, only: [:index]
 
-  get '/movies' => 'movies#index'
-  get 'movies/new' => 'movies#new'
+  resources :movies
 
+  resources :albums
 
+  resources :books
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
