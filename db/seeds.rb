ed0a40098e3f8c722bc97549e6fcf6501fe20c52
Yasmin <yasminor@gmail.com>
2016-10-11 21:01:21 -0700

    Used Faker gem to pre-populate the database with fake movie, album and book data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+40.times do
+  m = Movie.new
+  m.name = Faker::Book.title # Random book title but I'll use it here for random movie titles
+  m.director = Faker::Name.name
+  m.description = Faker::Hipster.sentence
+  m.movie_ranking = 0
+
+  m.save
+end
+
+40.times do
+  b = Book.new
+  b.name = Faker::Book.title # Random book title
+  b.author = Faker::Book.author # Random author name
+  b.description = Faker::Book.genre # Random book genre
+  b.book_ranking = 0
+
+  b.save
+end
+
+40.times do
+  a = Album.new
+  a.name = Faker::Name.name
+  a.artist = Faker::Hipster.word # Random hipster word will work well for Artist
+  a.description = Faker::Lorem.sentence
+  a.album_ranking = 0
+
+  a.save
+end
